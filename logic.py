2fc61017eecfa0b322a2ade058162e0790e5a755
Enderman9424 <ruzgar.cinar0601@gmail.com>
2025-07-09 22:30:07 +0300

    Enhance Pokémon class with power and HP attributes; add attack command for battles

--- logic.py
+++ logic.py
@@ -9,6 +9,8 @@ class Pokemon:
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
         self.height = None
+        self.power = random.randint(30, 60)  # Pokémon'un gücünü rastgele bir değer olarak ayarlar
+        self.hp = random.randint(200, 400)  # Pokémon'un can puanını rastgele bir değer olarak ayarlar
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
@@ -16,7 +18,7 @@ class Pokemon:
 
     async def get_name(self):
         # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API 
         async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
             async with session.get(url) as response:  # GET isteği gönderme
                 if response.status == 200:
@@ -27,21 +29,24 @@ class Pokemon:
                 
     async def get_height(self):
         # PokeAPI aracılığıyla bir pokémonun yüksekliğini almak için asenktron metot
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}' # İstek için URL API
         async with aiohttp.ClientSession() as session:  # HTTP oturumu açma
-            async with session.get(url) as response:
+            async with session.get(url) as response: # GET isteği gönderme
                 if response.status == 200:
-                    data = await response.json()
-                    return data['height']
-                else:
+                    data = await response.json() # JSON yanıtının alınması ve çözümlenmesi
+                    return data['height'] # Pokémon'un yüksekliğini döndürür
+                else: 
                     return random.randint(1, 20)  # İstek başarısız olursa rastgele bir yükseklik döndürür
 
     async def info(self):
         # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
             self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-            self.height = await self.get_height()
-        return f"Pokémonunuzun ismi: {self.name} yüksekliği: {self.height}"  # Pokémon adını içeren dizeyi döndürür
+            self.height = await self.get_height() # Henüz yüklenmemişse bir yüksekliğin geri alınması
+        return f"""Pokémonunuzun İsmi: {self.name}
+        Canı: {self.hp}
+        Güç: {self.power}
+        Yüksekliği: {self.height}""" # Pokémon hakkında bilgi döndürür
 
     async def show_img(self):
         # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için asenktron metot
@@ -54,3 +59,29 @@ class Pokemon:
                     return img_url  # Pokémon görüntüsünün URL'sini döndürür
                 else:
                     return None
+                
+    async def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = random.randint(1, 5)
+            if chance == 1:
+                return "Sihirbaz Pokémon, savaşta bir kalkan kullandı!"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu şimdi {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+    
+                
+
+                
+class Wizard(Pokemon):
+    pass
+    
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        super_power = random.randint(5, 15)
+        self.power += super_power
+        result = await super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nDövüşçü Pokémon süper saldırı kullandı. Eklenen güç: {super_power}"
